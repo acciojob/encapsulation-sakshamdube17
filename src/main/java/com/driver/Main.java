9ee900f59f72e4862b860f5b8e21772d31306774
Saksham Dube <sakshamdube17@gmail.com>
2024-12-11 15:05:00 +0530

    done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly rwOnly=new RWOnly();
+//        rwOnly.s="AbcD";
+//        System.out.println(rwOnly.s);
+
+        rwOnly.setS("Saksham");
+        System.out.println(rwOnly.getS());
+    }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private String s ;
+
+    public String getS() {
+        return s;
+    }
+
+    public void setS(String s) {
+        this.s = s;
+    }
 }
